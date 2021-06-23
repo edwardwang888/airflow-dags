444a17b00f1962b08885eec91b505766838b416e
Edward Wang <edward.wang@deserve.com>
2021-06-22 21:34:03 -0700

    Add parametrized branch operator

---
+++ branch_operator_parametrized.py
@@ -0,0 +1,44 @@
+"""
+DAG containing a parametrized branch operator. Parameters are
+provided as member variables of the class.  If it is possible
+pass parameters directly to `context`, that would be preferred.
+"""
+
+from airflow import DAG
+from airflow.operators.dummy import DummyOperator
+from airflow.operators.branch import BaseBranchOperator
+from airflow.utils.dates import days_ago
+
+
+class ParametrizedBranchOperator(BaseBranchOperator):
+    def __init__(self, *args, myvar, downstream_tasks, **kwargs):
+        self.myvar = myvar
+        self.downstream_tasks = downstream_tasks
+        super().__init__(*args, **kwargs)
+
+    def choose_branch(self, context):
+        print(f'myvar is: {self.myvar}')
+        return self.downstream_tasks if self.myvar else []
+
+
+if __name__ == '__main__':
+    dag = DAG(
+        dag_id='parametrized_branch_operator',
+        schedule_interval=None,
+        start_date=days_ago(0),
+        catchup=False
+    )
+
+    dummy = DummyOperator(
+        task_id='dummy',
+        dag=dag
+    )
+
+    branch = ParametrizedBranchOperator(
+        task_id='branch',
+        myvar=0,
+        downstream_tasks=dummy.task_id,
+        dag=dag
+    )
+
+    branch >> dummy
